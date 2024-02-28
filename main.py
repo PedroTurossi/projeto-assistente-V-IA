921786ead9550b32843c2aaf30ea1966ea853592
PedroTurossi <pedrohenriqueturo@gmail.com>
2024-02-28 17:58:17 -0300

    criando classe e adicionando movimento

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ class Assistente:
         self.label.pack()
 
         # faz a janela ser sem bordas e transparente
+        self.root.overrideredirect(True)
         self.root.geometry("+50-50")
         self.root.lift()
         self.root.wm_attributes("-topmost", True)
@@ -18,20 +19,15 @@ class Assistente:
         self.label.bind("<Button-1>", self.on_drag_start)
         self.label.bind("<B1-Motion>", self.on_drag_motion)
 
-        self.label.bind("<Button-3>", self.teste1arroba)
-        self.root.mainloop()
-
+    # movimento
     def on_drag_start(self, event):
         self._start_x = event.x
         self._start_y = event.y
 
+    # movimento
     def on_drag_motion(self, event):
         new_x = self.root.winfo_x() + (event.x - self._start_x)
         new_y = self.root.winfo_y() + (event.y - self._start_y)
         self.root.geometry(f"+{new_x}+{new_y}")
-
-    def teste1arroba(self, event):
-        print('teste1@')
-        self.label2 = tk.Label(self.root, text="teste1@", bg="#dddddd", fg="#333333")
 
 Assistente()
