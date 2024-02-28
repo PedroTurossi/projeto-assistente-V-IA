5570b5e1c97448bc78180b1363ee9d2dcb484a85
PedroTurossi <pedrohenriqueturo@gmail.com>
2024-02-28 17:55:55 -0300

    criando classe e adicionando movimento

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 import tkinter as tk
 
-root = tk.Tk()
+class Assistente:
+    def __init__(self):
+        # cria a janela espiritual e a imagem (que no futuro será uma animação real)
+        self.root = tk.Tk()
+        self.image = tk.PhotoImage(file="rajehfeliz.png")
+        self.label = tk.Label(self.root, image=self.image, bg='white')
+        self.label.pack()
 
-# Carrega a imagem
-image = tk.PhotoImage(file="rajehfeliz.png")
+        # faz a janela ser sem bordas e transparente
+        self.root.geometry("+50-50")
+        self.root.lift()
+        self.root.wm_attributes("-topmost", True)
+        self.root.wm_attributes("-transparentcolor", "white")
 
-# Cria um widget Label e define a imagem como seu conteúdo
-label = tk.Label(root, image=image, bg='white')
-label.pack()
+        # adiciona a funcionalidade de arrastar o label
+        self.label.bind("<Button-1>", self.on_drag_start)
+        self.label.bind("<B1-Motion>", self.on_drag_motion)
 
-# Configura a janela para ser sem bordas e transparente
-root.overrideredirect(True)
-root.geometry("-200+250")
-root.lift()
-root.wm_attributes("-topmost", True)
-root.wm_attributes("-disabled", True)
-root.wm_attributes("-transparentcolor", "white")
+        self.label.bind("<Button-3>", self.teste1arroba)
+        self.root.mainloop()
 
-root.mainloop()
+    def on_drag_start(self, event):
+        self._start_x = event.x
+        self._start_y = event.y
+
+    def on_drag_motion(self, event):
+        new_x = self.root.winfo_x() + (event.x - self._start_x)
+        new_y = self.root.winfo_y() + (event.y - self._start_y)
+        self.root.geometry(f"+{new_x}+{new_y}")
+
+    def teste1arroba(self, event):
+        print('teste1@')
+        self.label2 = tk.Label(self.root, text="teste1@", bg="#dddddd", fg="#333333")
+
+Assistente()
