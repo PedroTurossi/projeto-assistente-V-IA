2fe40d9389a730199b8faecd15d33b5ffd01c715
PedroTurossi <pedrohenriqueturo@gmail.com>
2024-03-11 18:43:05 -0300

    adição de arquivos (sem funcionalidade em conjunto)

---
+++ spotify_conex.py
@@ -0,0 +1,66 @@
+from flask import Flask, request, url_for, session, redirect
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import time
+
+app = Flask(__name__)
+
+
+app.secret_key = 'Ogj53kSFk9875'
+app.config['SESSION_COOKIE_NAME'] = 'Cookie'
+TOKEN_INFO = 'token_info'
+
+@app.route('/')
+def login():
+    sp_oauth = create_spotify_oath()
+    auth_url = sp_oauth.get_authorize_url()
+    return redirect(auth_url)
+
+@app.route('/redirect')
+def redirectPage():
+    sp_oauth = create_spotify_oath()
+    session.clear()
+    code = request.args.get('code')
+    token_info = sp_oauth.get_access_token(code)
+    session[TOKEN_INFO] = token_info
+    return redirect(url_for('getTracks', _external=True))
+
+@app.route('/getTracks')
+def getTracks():
+    try:
+        token_info = get_token()
+    except:
+        print('usuário não logado')
+        redirect('login', _external=False)
+    sp = spotipy.Spotify(auth=token_info['access_token'])
+    
+
+    try:
+        return sp.current_user_playing_track()
+        return "Música iniciada com sucesso"
+    except spotipy.exceptions.SpotifyException as e:
+        return f"Erro ao iniciar a música: {e}"
+
+def get_token():
+    token_info = session.get(TOKEN_INFO, None)
+    if not token_info:
+        raise 'exception'
+    now = int(time.time())
+    is_expired = token_info['expires_at'] - now < 60
+    if (is_expired):
+        sp_oauth = create_spotify_oath()
+        token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
+    return token_info
+
+def busca_musica():
+    token_info = get_token()
+    sp = spotipy.Spotify(auth=token_info['access_token'])
+    
+
+def create_spotify_oath():
+    return SpotifyOAuth(
+        client_id = ' ',
+        client_secret = ' ',
+        redirect_uri=url_for('redirectPage', _external=True),
+        scope='user-library-read user-read-currently-playing user-modify-playback-state user-read-playback-state app-remote-control'
+    )
