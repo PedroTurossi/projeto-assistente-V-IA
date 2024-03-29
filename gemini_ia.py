60a38e7cbd1acfe25a36bba0ac690dcdd36974fe
PedroTurossi <pedrohenriqueturo@gmail.com>
2024-03-28 21:41:31 -0300

    IA agora é uma classe e delete da API o spotify (ela n pode ser usada para as ideias do projeto)

--- gemini_ia.py
+++ gemini_ia.py
@@ -1,44 +1,27 @@
 import speech_recognition as sr
-import pyttsx3
 import google.generativeai as genai
 from configparser import ConfigParser
 
-def ouvir():
-    microfone = sr.Recognizer()
+class IA():
+    def __init__(self):
+        config = ConfigParser()
+        config.read('chave_google.ini')
+        self.api_key = config['API_KEY']['google_api_key']
+        genai.configure(api_key=self.api_key)
 
-    with sr.Microphone() as source:
-        microfone.adjust_for_ambient_noise(source)
-        print('o rajeh é ')
-        audio = microfone.listen(source)
-    try:
-        frase = microfone.recognize_google(audio, language = "PT-BR")
-        print('voce falo:' + frase)
-        return frase
-    except sr.UnknownValueError:
-        print('repete mais devegar')
-        return None
-def chatbot(texto):
-    config = ConfigParser()
-    config.read('chave_google.ini')
-    api_key = config['API_KEY']['google_api_key']
-    genai.configure(api_key=api_key)
-    model_gemini = genai.GenerativeModel('gemini-pro')
-    promp = "--- "+texto+" (no maximo um paragrafo)---"
-    resposta = model_gemini.generate_content(promp)
-    return resposta.text
-def falar(texto):
-    fala = texto
-    speaker = pyttsx3.init()
-    voices = speaker.getProperty('voices')
-    speaker.setProperty('voice', voices[0].id)
-    rate = speaker.getProperty('rate')
-    speaker.setProperty('rate', rate - 25)
-    speaker.say(fala)
-    print(fala)
-    speaker.runAndWait()
+    def responder(self, pergunta):
+        model_gemini = genai.GenerativeModel('gemini-pro')
+        promp = "--- "+ pergunta +" (no maximo um paragrafo)---"
+        resposta = model_gemini.generate_content(promp)
+        return resposta.text
 
-
-frase = ouvir()
-conversa =chatbot(frase)
-falar(conversa) 
-print(conversa)
+    def ouvir_usuario(self):
+        microfone = sr.Recognizer()
+        with sr.Microphone() as source:
+            microfone.adjust_for_ambient_noise(source)
+            audio = microfone.listen(source)
+        try:
+            frase = microfone.recognize_google(audio, language = "PT-BR")
+            return frase
+        except sr.UnknownValueError:
+            return None
